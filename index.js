fd30f8441f820ed6645772d15a567a0cb9896287
Luca <lucakanyo@protonmail.com>
2020-09-02 13:10:38 -0400

    Done.

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ class Formatter {
     return string.replace(/[^-, 'A-Za-z0-9]+/g, '');
   } //added an extra space here^
 
-  static titleize(sentence){
+
+  static titleize(string) {
+    let words = string.split(' ');
     const skips = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
-    let result = sentence.split(' ').map(word => {
-      if(word !== sentence[0] && skips.includes(word)){
-        return word
-      } else {
-        return word.charAt(0).toUpperCase + word.slice(1)
+    for (let i = 0; i < words.length; i++) {
+      if (words[i] === string[0] || !skips.includes(words[i])){
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
       }
     }
-      )
-      return result.join(" ")
+    return words.join(' ');
   }
+
 }
+
